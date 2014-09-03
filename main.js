aeb877b33b19824b5a6895977f3b47b1b12be092
Manuel Valls Fernández <manolo@chatiku.com>
2014-09-03 11:22:50 +0200

    Introduced vz.property

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-#!/usr/bin/env node
-
-//-------------------------------//
-// © 2014 Manuel Valls Fernández //
-// manolovalls@gmail.com         //
-// Todos los derechos reservados //
-// All rights reserved           //
-//-------------------------------//
+var Property = require('vz.property'),
+    
+    worker = new Property(),
+    url = new Property(),
+    ready = new Property(),
+		awaiting = new Property(),
+		tasks = new Property(),
+		nextTag = new Property();
 
 function onMessage(e){
-	var task = this.that._tasks[e.data[1]];
-	
+	var task = tasks.of(this.that).get()[e.data[1]];
 	task.cb.call(task.that,e.data[0]);
 }
 
 function onReady(e){
-	this.that._ready = true;
+	ready.of(this.that).set(true);
 	this.onmessage = onMessage;
 	
 	var args;
-	while(args = this.that._awaiting.shift()) this.that.run.apply(this.that,args);
+	while(args = awaiting.of(this.that).get().shift()) this.that.run.apply(this.that,args);
 }
 
 function Thread(setup,handler){
@@ -47,34 +46,30 @@ function Thread(setup,handler){
 	txt += 'postMessage("ready");'
 	
 	var blob = new Blob([txt],{type: 'text/javascript'}),
-			url = URL.createObjectURL(blob),
-			worker = new Worker(url);
+			u = URL.createObjectURL(blob),
+			w = new Worker(url);
 	
-	worker.onmessage = onReady;
+	w.onmessage = onReady;
+	w.that = this;
 	
-	Object.defineProperties(worker,{
-		that: {value: this}
-	});
-	
-	Object.defineProperties(this,{
-		_worker: {value: worker},
-		_url: {value: url},
-		_ready: {value: false,writable: true},
-		_awaiting: {value: []},
-		_tasks: {value: {}},
-		_nextTag: {value: 0,writable: true}
-	});
+  worker.of(this).set(w);
+  url.of(this).set(u);
+  ready.of(this).set(false);
+  awaiting.of(this).set([]);
+  tasks.of(this).set({});
+  nextTag.of(this).set(0);
+  
 }
 
 module.exports = Thread;
 
 Thread.prototype.run = function(data,trans,callback,that){
-	if(!this._ready){
-		this._awaiting.push(arguments);
+	if(!ready.of(this).get()){
+    awaiting.of(this).get().push(arguments);
 		return;
 	}
 	
-	var tag = this._nextTag++;
+	var tag = nextTag.of(this).value++;
 	
 	if(typeof trans == 'function'){
 		that = callback;
@@ -82,14 +77,14 @@ Thread.prototype.run = function(data,trans,callback,that){
 		trans = null;
 	}
 	
-	if(trans) this._worker.postMessage([data,tag],trans);
-	else this._worker.postMessage([data,tag]);
+	if(trans) worker.of(this).get().postMessage([data,tag],trans);
+	else worker.of(this).get().postMessage([data,tag]);
 	
-	this._tasks[tag] = {cb: callback,that: that || this};
+	tasks.of(this).get()[tag] = {cb: callback,that: that || this};
 };
 
 Thread.prototype.destroy = function(){
-	this._worker.terminate();
-	URL.revokeObjectURL(this._url);
+	worker.of(this).get().terminate();
+	URL.revokeObjectURL(url.of(this).get());
 };
 
